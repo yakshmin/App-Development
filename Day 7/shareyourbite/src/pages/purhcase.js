8eb3753ef4d7ebe986aaa79b63696f448b38cbae
yakshmin <120238607+yakshmin@users.noreply.github.com>
2023-09-27 14:51:50 +0530

    Add files via upload

---
+++ Day 7/shareyourbite/src/pages/purhcase.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from '../components/navbar';
+
+const Purchase = () => {
+return (
+	<div
+	style={{
+		
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: '100px'
+	}}
+	>
+		<Navbar></Navbar>
+	<h1 className='texts'>Purchase Food</h1>
+	</div>
+);
+};
+
+export default Purchase;
